2f086b1b9dda69260310f8cd57c45b5b321605ed
Michael Möschl-Brandl <mmoeschlbrandl@eurofunk.com>
2026-03-25 17:07:13 +0100

    fix: Initialize items

--- Java/src/main/java/com/gildedrose/GildedRoseConfiguration.java
+++ Java/src/main/java/com/gildedrose/GildedRoseConfiguration.java
@@ -45,6 +45,13 @@ public class GildedRoseConfiguration {
 
     @Bean
     public GildedRose gildedRose() {
-        return new GildedRose(new Item[0]);
+        return new GildedRose(new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Aged Brie", 2, 0),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Conjured Mana Cake", 3, 6)
+        });
     }
 }
